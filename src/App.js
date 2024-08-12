cb14f4af08d2313a8de1e78a2c826db11baa1519
ayecaptain <pkemoimethodius@gmail.com>
2024-08-12 15:42:51 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Hero from './components/Hero';
 import './App.css'
 
 function App() {
+
   return (
     <div>
        <Hero/>
       <NavBar/>
-     
+
     </div>
   )
 }
 
 export default App;
+
+
